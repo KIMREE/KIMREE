787fc97bd30a7fa88973b4e5efdb059e74e9e596
KIMREE <cool@cooldeMacBook.local>
2014-04-12 00:24:35 +0800

    1.48
    登陆界面修改

--- KIMREE/MyshopViewController.m
+++ KIMREE/MyshopViewController.m
@@ -23,25 +23,33 @@
     return self;
 }
 
+
+-(void)checkboxClick:(UIButton *)btn
+{
+    btn.selected = !btn.selected;
+}
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
     self.view.backgroundColor=[UIColor grayColor];
-    UITextField *userbox=[[UITextField alloc] initWithFrame:CGRectMake(30, 170, 260, 30)];
-    UITextField  *passwordbox=[[UITextField alloc] initWithFrame:CGRectMake(30, 210, 260, 30)];
+    UITextField *userbox=[[UITextField alloc] initWithFrame:CGRectMake(30, 180, 260, 30)];
+    UITextField  *passwordbox=[[UITextField alloc] initWithFrame:CGRectMake(30, 230, 260, 30)];
     
-    [userbox setBorderStyle:UITextBorderStyleRoundedRect]; //外框类型
-    userbox.placeholder = @"email"; //默认显示的字
+    [userbox setBorderStyle:UITextBorderStyleRoundedRect];
+    userbox.placeholder = @"email";
     userbox.autocorrectionType = UITextAutocorrectionTypeNo;
     userbox.autocapitalizationType = UITextAutocapitalizationTypeNone;
     userbox.returnKeyType = UIReturnKeyDone;
     userbox.clearButtonMode = UITextFieldViewModeWhileEditing; //编辑时会出现个修改X
   
-    [passwordbox setBorderStyle:UITextBorderStyleRoundedRect]; //外框类型
-    passwordbox.placeholder = @"password"; //默认显示的字
-    passwordbox.secureTextEntry = YES; //密码
+    [passwordbox setBorderStyle:UITextBorderStyleRoundedRect];
+    passwordbox.placeholder = @"password";
+    passwordbox.secureTextEntry = YES;
     passwordbox.autocorrectionType = UITextAutocorrectionTypeNo;
     passwordbox.autocapitalizationType = UITextAutocapitalizationTypeNone;
     passwordbox.returnKeyType = UIReturnKeyDone;
@@ -49,8 +57,21 @@
     
   
     [self.view addSubview:userbox];
-     [self.view addSubview:passwordbox];
+    [self.view addSubview:passwordbox];
+    
+    UIButton *checkbox = [UIButton buttonWithType:UIButtonTypeCustom];
+    
+    CGRect checkboxRect = CGRectMake(30, 285, 20, 20);
+    [checkbox setFrame:checkboxRect];
+    
+    [checkbox setImage:[UIImage imageNamed:@"checkbox_out.png"] forState:UIControlStateNormal];
+    [checkbox setImage:[UIImage imageNamed:@"checkbox_in.png"] forState:UIControlStateSelected];
+    
+    [checkbox addTarget:self action:@selector(checkboxClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:checkbox];
+    
 }
+
 
 - (void)didReceiveMemoryWarning
 {
