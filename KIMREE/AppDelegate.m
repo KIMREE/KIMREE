651e2e1d8e624bcde0df0a7d84d9f7e8de69235a
KIMREE <renchunyu@renchunyudeiMac.local>
2014-04-14 09:40:10 +0800

    1.47

--- KIMREE/AppDelegate.m
+++ KIMREE/AppDelegate.m
@@ -31,13 +31,10 @@
         style.font = [UIFont systemFontOfSize:12];
         return style;
     }];
-<<<<<<< HEAD
-   
-    //初始化主视图
-=======
+
 
   //初始化主视图
->>>>>>> master
+
     self.rootController = [[MainViewController alloc]initWithNibName:@"MainViewController" bundle:nil];
     self.navController = [[UINavigationController alloc]initWithRootViewController:_rootController];
     self.navController.navigationBar.hidden = YES;
